1fbacb3fac3970f1ad74096e7f9e970675b5d6a8
Miguel Gallego <1636242@uab.cat>
2026-02-02 15:59:38 +0100

    Eliminar write button + steps acabados

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -4,7 +4,6 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/Search/SearchPage";
-import WritePage from "./pages/WritePage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useUser } from "@/contexts/UserContext";
 import "../curation-pipeline/src/index.css";
@@ -21,13 +20,6 @@ function AppRoutes() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="Search/:step?" element={<SearchPage />} />
-          <Route
-            path="Write"
-            element={
-              <ProtectedRoute userStatus={userStatus}>
-              </ProtectedRoute>
-            }
-          />
         </Route>
 
         <Route
